976dc2e822c87f3fd75516ceaf175ab6459e795a
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-02-12 23:17:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const base_url =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropdowns = document.querySelectorAll("#dropdown select");
+const amount = document.querySelector("#Amount");
+const fromCurr = document.querySelector("#from select");
+const toCurr = document.querySelector("#to select");
+const msg = document.querySelector(".msg");
+const btn = document.querySelector("button");
+
+for (let select of dropdowns) {
+  for (curCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = curCode;
+    newOption.value = curCode;
+    if (select.name == "from" && curCode == "USD") {
+      newOption.selected = "selected";
+    } else if (select.name == "to" && curCode == "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let curCode = element.value;
+  let countryCode = countryList[curCode];
+  let src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = src;
+};
+const getExchangeRate = async () => {
+  let amtVal = amount.value;
+  let response = await fetch(
+    `https://api.frankfurter.app/latest?amount=${amtVal}&from=${fromCurr.value}&to=${toCurr.value}`
+  );
+  let data = await response.json();
+  let finalAmt = amtVal * data.rates[toCurr.value];
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+};
+
+window.addEventListener("load", () => {
+  getExchangeRate();
+});
+
+btn.addEventListener("click", () => {
+  getExchangeRate();
+});

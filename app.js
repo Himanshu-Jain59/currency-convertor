f9d8d2fa4d69adfc936b85a704dddfab7fdbb88e
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-02-13 21:59:11 +0530

    cross btn event added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const base_url =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const cross = document.querySelector("#cross");
 const dropdowns = document.querySelectorAll("#dropdown select");
+const option = document.querySelectorAll("option");
 const amount = document.querySelector("#Amount");
 const fromCurr = document.querySelector("#from select");
 const toCurr = document.querySelector("#to select");
@@ -10,6 +13,7 @@ const btn = document.querySelector("button");
 for (let select of dropdowns) {
   for (curCode in countryList) {
     let newOption = document.createElement("option");
+    // console.log(newOption);
     newOption.innerText = curCode;
     newOption.value = curCode;
     if (select.name == "from" && curCode == "USD") {
@@ -18,6 +22,11 @@ for (let select of dropdowns) {
       newOption.selected = "selected";
     }
     select.append(newOption);
+
+    // cross.addEventListener("click", () => {
+    //   let from = toCurr.value;
+    //   let to = fromCurr.value;
+    // });
   }
   select.addEventListener("change", (evt) => {
     updateFlag(evt.target);
@@ -43,6 +52,17 @@ const getExchangeRate = async () => {
 
 window.addEventListener("load", () => {
   getExchangeRate();
+});
+
+cross.addEventListener("click", () => {
+  let from = toCurr.value;
+  let to = fromCurr.value;
+
+  fromCurr.value = from;
+  toCurr.value = to;
+
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
 });
 
 btn.addEventListener("click", () => {
